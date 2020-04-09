60063013eac3da2b8ecf200a6c1ed065af7f639b
saiakhil14 <60837524+saiakhil14@users.noreply.github.com>
2020-04-09 22:21:35 +0530

    Delete samp.js

--- samp.js
+++
@@ -1,8 +0,0 @@
-alert(" I repeate dont forget to touch the cake");
-
-window.addEventListener('click', function () {
-    
-    var audio = document.getElementById("HBD");
-    audio.play();
-    
-});
